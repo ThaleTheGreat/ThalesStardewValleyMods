cfb681345a2e953db62f186da9ec16d16d904e1a
Thale <wellsthegreat@gmail.com>
2026-05-15 15:06:43 -0500

    Add files via upload

---
+++ Calendar Master/Calendar Master Source/IGenericModConfigMenuApi.cs
@@ -0,0 +1,27 @@
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System;
+
+#nullable enable
+namespace CalendarMaster;
+
+public interface IGenericModConfigMenuApi
+{
+  void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
+
+  void AddBoolOption(
+    IManifest mod,
+    Func<bool> getValue,
+    Action<bool> setValue,
+    Func<string> name,
+    Func<string>? tooltip = null,
+    string? fieldId = null);
+
+  void AddKeybindList(
+    IManifest mod,
+    Func<KeybindList> getValue,
+    Action<KeybindList> setValue,
+    Func<string> name,
+    Func<string>? tooltip = null,
+    string? fieldId = null);
+}
