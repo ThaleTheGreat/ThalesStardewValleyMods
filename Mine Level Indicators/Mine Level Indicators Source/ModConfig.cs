f6b651189cf88806226faf2cd98c5df073166972
Thale <wellsthegreat@gmail.com>
2026-05-18 12:57:59 -0500

    Add files via upload

---
+++ Mine Level Indicators/Mine Level Indicators Source/ModConfig.cs
@@ -0,0 +1,8 @@
+namespace MineLevelIndicators;
+
+public sealed class ModConfig
+{
+    public bool ShowInfestedLevelIndicators { get; set; } = true;
+
+    public bool ShowMushroomLevelIndicators { get; set; } = true;
+}
