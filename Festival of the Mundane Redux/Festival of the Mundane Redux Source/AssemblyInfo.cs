b9166767c75b50efd36c01e3977f022c6cc42d01
Thale <wellsthegreat@gmail.com>
2026-05-18 12:22:47 -0500

    Update AssemblyInfo.cs

--- Festival of the Mundane Redux/Festival of the Mundane Redux Source/AssemblyInfo.cs
+++ Festival of the Mundane Redux/Festival of the Mundane Redux Source/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4b74ae78-e9d3-4991-b183-994d376bf3b4")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
-[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
