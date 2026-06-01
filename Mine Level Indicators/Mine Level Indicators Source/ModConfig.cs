264ff9232452d88751eef9400e53c66015aeeb55
Thale <wellsthegreat@gmail.com>
2026-05-31 22:30:19 -0500

    Delete Mine Level Indicators directory

--- Mine Level Indicators/Mine Level Indicators Source/ModConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace MineLevelIndicators;
-
-public sealed class ModConfig
-{
-    public bool ShowInfestedLevelIndicators { get; set; } = true;
-
-    public bool ShowMushroomLevelIndicators { get; set; } = true;
-}
