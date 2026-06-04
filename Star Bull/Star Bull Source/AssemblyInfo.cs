7ff15a86e676a6f871fe0e7e9fd7e4eaf690c66a
Thale <wellsthegreat@gmail.com>
2026-06-04 16:23:12 -0500

    Add files via upload

--- Star Bull/Star Bull Source/AssemblyInfo.cs
+++ Star Bull/Star Bull Source/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("ThaleTheGreat")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.3.4.0")]
-[assembly: AssemblyInformationalVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyInformationalVersion("1.4.1")]
 [assembly: AssemblyProduct("Star Bull")]
 [assembly: AssemblyTitle("Star Bull")]
-[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyVersion("1.4.1.0")]
