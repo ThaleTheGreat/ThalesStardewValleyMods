5c180591aa1adb4251348eab06e7c52290a8e5f1
Thale <wellsthegreat@gmail.com>
2026-06-07 01:35:25 -0500

    Add files via upload

---
+++ Wallet Tools/Wallet Tools/Wallet Tools Source/IWalletToolsApi.cs
@@ -0,0 +1,9 @@
+namespace ThaleTheGreat.WalletTools;
+
+public interface IWalletToolsApi
+{
+    int GetHighestStoredCoreToolLevel();
+    int GetStoredToolLevel(string toolKind);
+    bool TryGetStoredToolQualifiedItemId(string toolKind, out string qualifiedItemId);
+    bool TryGetStoredTool(string toolKind, out string qualifiedItemId, out int upgradeLevel, out string displayName);
+}
