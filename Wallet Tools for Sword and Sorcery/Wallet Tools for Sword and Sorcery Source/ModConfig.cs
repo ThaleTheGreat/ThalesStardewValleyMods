29b7f8e4ef5513a5c042c05aea28fd94a96b45ef
Thale <wellsthegreat@gmail.com>
2026-07-20 02:58:19 -0500

    Delete Wallet Tools for Sword and Sorcery directory

--- Wallet Tools for Sword and Sorcery/Wallet Tools for Sword and Sorcery Source/ModConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace ThaleTheGreat.WalletToolsForSwordAndSorcery;
-
-internal sealed class ModConfig
-{
-    public bool ModEnabled { get; set; } = true;
-    public bool PreferSwordAndSorcerySameTierTools { get; set; } = true;
-    public bool DebugLogging { get; set; } = false;
-}
