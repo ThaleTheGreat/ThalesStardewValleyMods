c9b3a2c1ec1684a0ebc759a4a158d0f440c19cdb
Thale <wellsthegreat@gmail.com>
2026-07-20 02:57:32 -0500

    Delete Wallet Auto Petter directory

--- Wallet Auto Petter/Wallet Auto Petter Source/SaveState.cs
+++
@@ -1,9 +0,0 @@
-namespace ThaleTheGreat.WalletAutoPetter;
-
-internal sealed class SaveState
-{
-    public bool HasAutoPetter { get; set; }
-    public int AutoPetterCount { get; set; }
-    public bool SuppressAutoStoreUntilInventoryAutoPetterLeaves { get; set; }
-}
-
