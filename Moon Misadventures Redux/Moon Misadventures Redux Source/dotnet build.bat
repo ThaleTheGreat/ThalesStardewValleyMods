8e5645977f6b29bfc5ac11d16cb60f5025f092c2
Thale <wellsthegreat@gmail.com>
2026-07-23 22:57:07 -0500

    Add files via upload

--- Moon Misadventures Redux/Moon Misadventures Redux Source/dotnet build.bat
+++ Moon Misadventures Redux/Moon Misadventures Redux Source/dotnet build.bat
@@ -2,6 +2,10 @@
 setlocal
 cd /d "%~dp0"
 
+if exist "bin" rmdir /s /q "bin"
+if exist "obj" rmdir /s /q "obj"
+if exist "release" rmdir /s /q "release"
+
 dotnet build
 set "exitCode=%errorlevel%"
 
